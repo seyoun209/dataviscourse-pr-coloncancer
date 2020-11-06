bf62255001e8a1a15fb4d28be0e0456a18d97a4b
Joon Park <hjoonpark.us@gmail.com>
2020-11-06 02:31:28 -0700

    combined qc data converted json

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ jsonNonpolyp.then(nonpolypData => {
     headers = nonpolypData.headers;
     data = nonpolypData.data;
 
-    data = data.slice(0, 20); // use only 20 data for quick debugging
+    data = data.slice(0, 20); // use only few data for quick debugging
     let tableNonpolyp = new TableNonpolyp(headers, data);
 });
 
@@ -13,6 +13,25 @@ jsonPolyp.then(polypData => {
     headers = polypData.headers;
     data = polypData.data;
 
-    data = data.slice(0, 20); // use only 20 data for quick debugging
+    data = data.slice(0, 20); // use only few data for quick debugging
     let tablePolyp = new TablePolyp(headers, data);
 });
+
+
+jsonCombinedQC = d3.json('./data/combined_qc.json').then(qcData => {
+    headers = qcData.headers;
+    console.log(headers);
+    data = qcData.data;
+    
+    data = data.slice(0, 3); // use only few data for quick debugging
+    for (let d of data) {
+        console.log("==============================");
+        for (let header of headers) {
+            if (header == "samples") {
+                console.log("    " + header + ":" + d[header].length + " samples");
+            } else {
+                console.log("    " + header + ":" + d[header]);
+            }
+        }
+    }
+})
